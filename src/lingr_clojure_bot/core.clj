48b51bb513ae588e33f709eeab730beedd00308d
ujihisa <ujihisa@gmail.com>
2012-01-19 09:32:49 -0800

    forces defining built-in functions every time

--- src/lingr_clojure_bot/core.clj
+++ src/lingr_clojure_bot/core.clj
@@ -6,18 +6,20 @@
     [clojail core])
   (:import java.util.concurrent.ExecutionException))
 
+(defn define-builtin-functions [sb]
+  (sb '(do
+         (defn what-time []
+           (.format (doto
+                      (java.text.DateFormat/getDateTimeInstance)
+                      (.setTimeZone (java.util.TimeZone/getTimeZone "PST")))
+                    (java.util.Date.)))
+         (defn sun []
+           "http://factoryjoe.s3.amazonaws.com/emoticons/emoticon-0157-sun.gif")
+         (defn rock []
+           "http://factoryjoe.s3.amazonaws.com/emoticons/emoticon-0178-rock.gif")
+         (defn locals [] (ns-interns *ns*)))))
+
 (def sb (sandbox #{}))
-(sb '(do
-       (defn what-time []
-         (.format (doto
-                    (java.text.DateFormat/getDateTimeInstance)
-                    (.setTimeZone (java.util.TimeZone/getTimeZone "PST")))
-                  (java.util.Date.)))
-       (defn sun []
-         "http://factoryjoe.s3.amazonaws.com/emoticons/emoticon-0157-sun.gif")
-       (defn rock []
-         "http://factoryjoe.s3.amazonaws.com/emoticons/emoticon-0178-rock.gif")
-       (defn locals [] (ns-interns *ns*))))
 
 (defn format-for-lingr [obj]
   (str (cond (seq? obj) (seq obj)
@@ -29,6 +31,7 @@
   (POST "/"
         {body :body}
         (let [message (:message (first (:events (read-json (slurp body)))))]
+          (define-builtin-functions sb)
           (sb (list 'def 'message message))
           (let [code (:text message)
                 expr (try (read-string code) (catch java.lang.RuntimeException e '()))]
