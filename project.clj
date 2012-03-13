fbdaf51e84e263fe5db3d2f69ff7ad9d05185e44
ujihisa <ujihisa@gmail.com>
2012-03-13 11:39:23 -0700

    cosmetic change

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject clojure-lingrbot "1.0.0-SNAPSHOT"
-            :description "FIXME: write description"
-            :main clojure-lingrbot.core
-            :dependencies [[org.clojure/clojure "1.3.0"]
-                           [org.clojure/data.json "0.1.2"]
-                           [compojure "1.0.0-RC2"]
-                           [ring "1.0.1"]
-                           [clojail "0.5.1"]])
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/data.json "0.1.2"]
+                 [compojure "1.0.0-RC2"]
+                 [ring "1.0.1"]
+                 [clojail "0.5.1"]]
+  :main clojure-lingrbot.core)
