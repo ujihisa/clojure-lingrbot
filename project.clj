2994720770a5067669a017059554c32c764162f6
ujihisa <ujihisa@gmail.com>
2012-04-25 19:49:38 -0700

    clojure version up

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clojure-lingrbot "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/data.json "0.1.2"]
                  [compojure "1.0.0-RC2"]
                  [ring "1.0.1"]
