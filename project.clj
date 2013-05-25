c8e1417a87f46c1f33930d66176826169d4aea8f
ujihisa <ujihisa@gmail.com>
2013-05-24 19:44:24 -0700

    expose jruby!

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
                  [org.clojure/data.json "0.1.2"]
                  [compojure "1.0.0-RC2"]
                  [ring "1.0.1"]
-                 [clojail "1.0.6"]]
+                 [clojail "1.0.6"]
+                 [org.jruby/jruby-complete "1.7.3"]]
   :main clojure-lingrbot.core
   :jvm-opts ["-Djava.security.policy=example.policy"])
